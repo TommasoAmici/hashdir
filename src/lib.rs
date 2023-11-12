748cd058599da65feb6709b9613f7eb4a865f39a
Tommaso Amici <me@tommasoamici.com>
2023-11-12 16:22:10 +0100

    refactor: moved logic to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
+use ignore::WalkBuilder;
+use std::fs;
+use std::io::Error;
+use std::path::PathBuf;
 use xxhash_rust::xxh3::xxh3_64;
 
-pub fn calculate_hash(input: &[u8]) -> u64 {
+fn calculate_hash(input: &[u8]) -> u64 {
     xxh3_64(input)
+}
+
+pub fn hashdir(directory: PathBuf, hidden_files: bool) -> Result<u64, Error> {
+    let mut hashes = String::new();
+    for result in WalkBuilder::new(directory).hidden(hidden_files).build() {
+        // Each item yielded by the iterator is either a directory entry or an
+        // error, so either print the path or the error.
+        match result {
+            Ok(entry) => {
+                if entry.path().is_file() {
+                    let content = fs::read(entry.path())?;
+                    let content_hash = calculate_hash(&content);
+                    let file_name = entry
+                        .path()
+                        .file_name()
+                        .expect("Failed to get file name")
+                        .to_str()
+                        .expect("Failed to convert file name to string");
+                    hashes.push_str(format!("{} {}\n", file_name, content_hash).as_str());
+                }
+            }
+            Err(err) => panic!("ERROR: {}", err),
+        }
+    }
+    Ok(calculate_hash(hashes.as_bytes()))
 }
